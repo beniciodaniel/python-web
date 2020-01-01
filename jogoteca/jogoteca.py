f63073dbeb4ad92122a99ed598daac0a31ab1ce1
Benicio <benicio.daniel@gmail.com>
2020-01-01 19:07:36 -0300

    Refactors the game list

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -2,10 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Game:
+    def __init__(self, name, category, console):
+        self.__name = name
+        self.__category = category
+        self.__console = console
 
-@app.route("/start")
-def hi():
-    return render_template('list.html')
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def category(self):
+        return self.__category
+
+    @property
+    def console(self):
+        return self.__console
+
+@app.route("/index")
+def index():
+
+    game1 = Game('Final Fantasy VII', 'RPG', 'PS1')
+    game2 = Game('Final Fantasy VI', 'RPG', 'SNES')
+    game3 = Game('Donkey Kong 2', 'Action', 'SNES')
+
+    list = [game1, game2, game3]
+    return render_template('list.html', title="Games", games=list)
 
 
 app.run()
